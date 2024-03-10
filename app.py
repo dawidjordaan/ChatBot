c3bd17679318431310df1c1730b05c014786c492
Dawid Jordaan <dawidjordaan1@gmail.com>
2024-03-10 10:52:54 +0200

    Added venv and altered gitignore

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-#code to go here
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot_response():
+    user_message = request.json['message']
+    return jsonify({"response": "You said: " + user_message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
